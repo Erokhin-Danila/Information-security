00c21b4b9a7356da81c7237302b797eded9ebd84
Danila <danilka.3rohin@yandex.ru>
2025-01-31 13:27:11 +0300

    Пересоздан репозиторий

---
+++ fiveLaba/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace fiveLaba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.password = new System.Windows.Forms.Button();
+            this.remove = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // password
+            // 
+            this.password.BackColor = System.Drawing.Color.Red;
+            this.password.Location = new System.Drawing.Point(71, 201);
+            this.password.Margin = new System.Windows.Forms.Padding(2);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(111, 49);
+            this.password.TabIndex = 0;
+            this.password.Text = "Сгенерировать пароль";
+            this.password.UseVisualStyleBackColor = false;
+            this.password.Click += new System.EventHandler(this.password_Click);
+            // 
+            // remove
+            // 
+            this.remove.BackColor = System.Drawing.Color.Lime;
+            this.remove.Location = new System.Drawing.Point(71, 271);
+            this.remove.Margin = new System.Windows.Forms.Padding(2);
+            this.remove.Name = "remove";
+            this.remove.Size = new System.Drawing.Size(116, 58);
+            this.remove.TabIndex = 1;
+            this.remove.Text = "Удалить пароль";
+            this.remove.UseVisualStyleBackColor = false;
+            this.remove.Click += new System.EventHandler(this.remove_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.BackColor = System.Drawing.Color.Aqua;
+            this.openFile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.openFile.Location = new System.Drawing.Point(66, 92);
+            this.openFile.Margin = new System.Windows.Forms.Padding(2);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(116, 43);
+            this.openFile.TabIndex = 2;
+            this.openFile.Text = "Открыть файл";
+            this.openFile.UseVisualStyleBackColor = false;
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(195, 160);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic Semilight", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(67, 159);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Путь к файлу:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(743, 457);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.openFile);
+            this.Controls.Add(this.remove);
+            this.Controls.Add(this.password);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button password;
+        private System.Windows.Forms.Button remove;
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ fiveLaba/Form1.cs
@@ -0,0 +1,111 @@
+﻿using GroupDocs.Merger;
+using GroupDocs.Merger.Domain.Options;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace fiveLaba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string name;
+        string secret_password;
+
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        
+        private void password_Click(object sender, EventArgs e)
+        {
+            // для повторной генерации пароля
+            Click_pov();
+
+            name = textBox1.Text;
+            Random rand = new Random();
+
+            int stringLength = rand.Next(7, 20);
+
+            int randValue;
+            string passwordLine = "";
+            char letter; // Один символ
+
+            for (int i = 0; i < stringLength; i++)
+            {
+                randValue = rand.Next(0, 26); // 26 букв английского алфавита
+                letter = Convert.ToChar(randValue + 97); // конвертация в символ Unicod(1 буква начинается c 97 символа)
+                passwordLine += letter; // Запись всех символов в переменную String
+            }
+
+            secret_password = passwordLine;
+            passwordLine = "";
+
+            // в файле содержится сгенерированный пароль
+            string path = "C:\\Users\\HP\\Рабочий стол\\семестр 5\\Основы теории безопасностти\\fiveLaba\\пароль.txt";
+
+            StreamWriter Password = new StreamWriter(path, false);
+            Password.WriteLine(secret_password);
+            Password.Close();
+
+            AddPasswordOptions addOptions = new AddPasswordOptions(secret_password);
+
+            if (name != "")
+            {
+                using (Merger merger = new Merger(name)) 
+                {
+                    merger.AddPassword(addOptions);
+                    merger.Save(name);
+                }
+            }
+        }
+            
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            name = textBox1.Text;
+
+            string path = "C:\\Users\\HP\\Рабочий стол\\семестр 5\\Основы теории безопасностти\\fiveLaba\\пароль.txt";
+
+            StreamReader reader = new StreamReader(path);
+            secret_password = reader.ReadLine();
+            reader.Close();
+
+            LoadOptions loadOptions = new LoadOptions(secret_password);
+
+            if (name != "")
+            {
+                using (Merger merger = new Merger(name, loadOptions))
+                {
+                    merger.RemovePassword();
+                    merger.Save(name);
+                }
+            }
+
+        }
+        // Выбираем какой файл открыть
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+                textBox1.Text = openFileDialog.FileName;
+        }
+
+        // После повторного нажатия для генерации пароля, очищаем содержимое файла
+        public void Click_pov()
+        {
+            string path = "C:\\Users\\HP\\Рабочий стол\\семестр 5\\Основы теории безопасностти\\fiveLaba\\пароль.txt";
+
+            StreamReader reader = new StreamReader(path);
+            secret_password = reader.ReadLine();
+            reader.Close();
+            if (secret_password != "")
+            {
+                StreamWriter Password = new StreamWriter(path, false);
+                Password.Write(string.Empty);
+                Password.Close();
+            }
+        }
+    }
+}  
